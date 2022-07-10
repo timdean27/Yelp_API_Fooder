60cef54e4ae6b1c207ecda9e8063384ce15e494b
timdean27 <timdean27@yahoo.com>
2022-07-10 14:08:17 -0400

    API back up

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ app.use(cors());
 
 const API_BASE_URL = process.env.API_BASE_URL
 const YELP_API_KEY = process.env.YELP_API_KEY
-const path = "/businesses/search"
-const searchCriteria = ({term: 'Pizza', location: 'New York', price : 2 , radius: 8049});
-const searchQuery = queryString.stringify(searchCriteria);
+// const path = "/businesses/search"
+// const searchCriteria = ({term: 'Pizza', location: 'New York', price : 2 , radius: 8049});
+// const searchQuery = queryString.stringify(searchCriteria);
 
 app.get('/api', (req, res) => {
-let recivedQuery = (req.query.searchCriteria)
-console.log("req.query.searchCriteria", recivedQuery)
-console.log("queryString.stringify(req.query.searchCriteria)", queryString.stringify(recivedQuery))
-console.log("searchQuery" ,searchQuery)
+let recivedQuery = req.query.searchQuery
+let path = req.query.path
+console.log("req.query.searchQuery", recivedQuery)
+
 
     const options = {
         method: 'GET',
-        url: `${API_BASE_URL}${path}?${searchQuery}`,
+        url: `${API_BASE_URL}${path}?${recivedQuery}`,
         headers: {
             Authorization: `Bearer ${YELP_API_KEY}`,
             Origin: "localhost",
