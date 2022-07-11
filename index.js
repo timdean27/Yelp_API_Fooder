39a13f5444a94bef950097752b803548e2e8be8f
timdean27 <timdean27@yahoo.com>
2022-07-10 23:41:44 -0400

    going to add addtional aPI calls for details and reviews

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios')
-const queryString = require('query-string');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const queryString = require("query-string");
+require("dotenv").config();
 const app = express();
-
 app.use(cors());
 
+////////////////////////////////API call For "/businesses/search"
+const API_BASE_URL = process.env.API_BASE_URL;
+const YELP_API_KEY = process.env.YELP_API_KEY;
+
+app.get("/api", (req, res) => {
+  let recivedQuery = req.query.searchQuery;
+  let path = req.query.path;
+  console.log("req.query.searchQuery", recivedQuery);
+
+  const options = {
+    method: "GET",
+    url: `${API_BASE_URL}${path}?${recivedQuery}`,
+    headers: {
+      Authorization: `Bearer ${YELP_API_KEY}`,
+      Origin: "localhost",
+      withCredentials: true,
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+////////////////////////////////API call For "/businesses/{id}"
+
+////////////////////////////////API call For "/businesses/{id}/reviews"
 
 
-const API_BASE_URL = process.env.API_BASE_URL
-const YELP_API_KEY = process.env.YELP_API_KEY
-// const path = "/businesses/search"
-// const searchCriteria = ({term: 'Pizza', location: 'New York', price : 2 , radius: 8049});
-// const searchQuery = queryString.stringify(searchCriteria);
-
-app.get('/api', (req, res) => {
-let recivedQuery = req.query.searchQuery
-let path = req.query.path
-console.log("req.query.searchQuery", recivedQuery)
 
 
-    const options = {
-        method: 'GET',
-        url: `${API_BASE_URL}${path}?${recivedQuery}`,
-        headers: {
-            Authorization: `Bearer ${YELP_API_KEY}`,
-            Origin: "localhost",
-            withCredentials: true,
-          },
-    }
 
-    axios.request(options).then((response) => {
-        res.json(response.data)
-        // console.log(response.data)
-    }).catch((error) => {
-        console.log(error)})
 
-})
 
-app.use(cors());
-// Create a variable for our port
 const port = process.env.PORT || 3500;
 
 // Run our server!
 app.listen(port, () => {
-  console.log(`Express MVC app is running on port ${port}`);
+  console.log(`Express MVC app is running on port ${port}`);
 });
